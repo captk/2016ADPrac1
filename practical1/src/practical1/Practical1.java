8663c93c81570cbfc5395eb5baccf3ee73648697
k <k@debian>
2016-03-11 02:45:50 +1030

    did task 4

--- practical1/src/practical1/Practical1.java
+++ practical1/src/practical1/Practical1.java
@@ -5,6 +5,7 @@
  */
 package practical1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,26 +17,41 @@ public class Practical1 {
     /**
      * @param args the command line arguments
      */
+    static ArrayList<Line> lineList = new ArrayList();
+    static int lineNum = 1;
+    static boolean moreLines = true;
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner in = new Scanner(System.in);
-        Line msg1, msg2, msg3;
-        int lineNum = 1;
-        
-        System.out.println(lineNum + ": Enter a line");
-        msg1 = new Line(in.nextLine(), lineNum);
-        lineNum++;
 
-        System.out.println(lineNum + ": Enter another line");
-        msg2 = new Line(in.nextLine(), lineNum);
-        lineNum++;
+        while (moreLines) {
+            askForLines();
+            addNewLine();
+            //System.out.println("while loop continues");
 
-        System.out.println(lineNum + ": Enter the last line");
-        msg3 = new Line(in.nextLine(), lineNum);
-        lineNum++;
+        }
 
-        System.out.println(msg3.getLineOfTextPretty() + "," + msg2.getLineOfTextPretty()
-                + "," + msg1.getLineOfTextPretty());
+        for (int i = lineList.size(); i > 0; i--) {
+            System.out.println(lineList.get(i - 1).getLineOfTextPretty());
+        }
+
     }
 
+    private static void askForLines() {
+        System.out.println(lineNum + ": Enter a line");
+    }
+
+    private static void addNewLine() {
+        String line = in.nextLine();
+
+        if (line.equals("")) {
+            //System.out.println(lineNum + ": no more lines");
+            moreLines = false;
+            return;
+        }
+        lineList.add(new Line(line, lineNum));
+        lineNum++;
+        //System.out.println("new line added and lineNum incremented");
+    }
 }
